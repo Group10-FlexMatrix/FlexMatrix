83e0c4c4f2a50d419111de58ffbcd3994bfb66cf
tuilakhanh <bacdau852@gmail.com>
2024-03-30 21:57:15 +0700

    feat: Move MainPanel to separate class

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
-qq
+
 tasks.test {
     useJUnitPlatform()
 }
