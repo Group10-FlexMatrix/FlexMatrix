eb4466028aaea3c844c023213f44d8b246212249
tuilakhanh <bacdau852@gmail.com>
2024-03-30 22:16:08 +0700

    feat: Implement set size

--- src/main/java/io/github/group10/flex/matrix/UI/MatrixInputPanel.java
+++ src/main/java/io/github/group10/flex/matrix/UI/MatrixInputPanel.java
@@ -14,8 +14,10 @@ public class MatrixInputPanel extends JPanel {
     private final String matrixName;
     private JTextArea matrixInputArea;
     private JScrollPane scrollPane;
-    private JButton randomButton, clearButton, importFromFileButton, parseMatrixButton;
+    private JButton randomButton, clearButton, importFromFileButton, parseMatrixButton, setSizeButton;
     private int[][] matrixValues;
+    private int rows = 0, cols = 0;
+    private boolean hasSetSize = false;
 
     public MatrixInputPanel(String matrixName) {
         this.matrixName = matrixName;
@@ -34,6 +36,7 @@ public class MatrixInputPanel extends JPanel {
         scrollPane = new JScrollPane(matrixInputArea);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
+        setSizeButton = new JButton("Set Size");
         randomButton = new JButton("Generate Random");
         importFromFileButton = new JButton("Import from File");
         parseMatrixButton = new JButton("Parse Matrix");
@@ -45,6 +48,7 @@ public class MatrixInputPanel extends JPanel {
 
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(randomButton);
+        buttonPanel.add(setSizeButton);
         buttonPanel.add(importFromFileButton);
         buttonPanel.add(parseMatrixButton);
         buttonPanel.add(clearButton);
@@ -52,21 +56,20 @@ public class MatrixInputPanel extends JPanel {
     }
 
     private void addListeners() {
+        setSizeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inputMatrixSize();
+            }
+        });
+
+
         randomButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int rows = getMatrixSize("Enter number of rows:");
-                if (rows == -1) return;
-
-                int cols = getMatrixSize("Enter number of columns:");
-                if (cols == -1) return;
-
-                if (rows <= 0 || cols <= 0) {
-                    JOptionPane.showMessageDialog(MatrixInputPanel.this, "Invalid matrix dimensions. Please enter positive values for rows and columns.",
-                            "Error Matrix " + matrixName, JOptionPane.ERROR_MESSAGE);
-                    return;
+                if (!hasSetSize) {
+                    if (inputMatrixSize() == -1) return;
                 }
-
                 matrixValues = Utils.randomMatrixParallel(rows, cols, 0, 9);
                 matrixInputArea.setText(Utils.matrixToString(matrixValues));
             }
@@ -105,6 +108,22 @@ public class MatrixInputPanel extends JPanel {
                 matrixValues = parseMatrix();
             }
         }));
+    }
+
+    private int inputMatrixSize() {
+        rows = getMatrixSize("Enter number of rows:");
+        if (rows == -1) return -1;
+
+        cols = getMatrixSize("Enter number of columns:");
+        if (cols == -1) return -1;
+
+        if (rows <= 0 || cols <= 0) {
+            JOptionPane.showMessageDialog(MatrixInputPanel.this, "Invalid matrix dimensions. Please enter positive values for rows and columns.",
+                    "Error Matrix " + matrixName, JOptionPane.ERROR_MESSAGE);
+            return -1;
+        }
+        hasSetSize = true;
+        return 1;
     }
 
     private int getMatrixSize(String message) {
