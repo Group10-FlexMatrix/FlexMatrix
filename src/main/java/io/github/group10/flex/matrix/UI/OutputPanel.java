0f3ce12cd8143016332f54b432cadd51bc6ed40c
Daniel Phan <danielphan.2003@gmail.com>
2024-03-31 16:04:40 +0700

    wip(strassen): init
    Signed-off-by: Daniel Phan <danielphan.2003@gmail.com>

--- src/main/java/io/github/group10/flex/matrix/UI/OutputPanel.java
+++ src/main/java/io/github/group10/flex/matrix/UI/OutputPanel.java
@@ -1,5 +1,6 @@
 package io.github.group10.flex.matrix.UI;
 
+import io.github.group10.flex.matrix.Model.MultiplicationInput;
 import io.github.group10.flex.matrix.Utils.Benchmark;
 import io.github.group10.flex.matrix.Utils.Utils;
 import io.github.group10.flex.matrix.Algorithm.*;
@@ -9,9 +10,9 @@ import java.awt.*;
 import java.util.concurrent.ForkJoinPool;
 
 public class OutputPanel extends JPanel {
-    private JButton computeNaiveButton, computeParallelButton, clearButton;
+    private JButton computeNaiveButton, computeParallelButton, computeStrassenButton, clearButton;
     private final MatrixInputPanel matrixA, matrixB;
-    private MatrixOutputPanel naiveOutput, parallelOutput;
+    private MatrixOutputPanel naiveOutput, parallelOutput, strassenOutput;
     private JLabel matrixMulInfoLabel;
 
     public OutputPanel(MatrixInputPanel matrixA, MatrixInputPanel matrixB) {
@@ -28,10 +29,12 @@ public class OutputPanel extends JPanel {
     private void createComponents() {
         computeNaiveButton = new JButton("Compute Naive");
         computeParallelButton = new JButton("Compute Parallel");
+        computeStrassenButton = new JButton("Compute Strassen");
         clearButton = new JButton("Clear");
 
         naiveOutput = new MatrixOutputPanel("Naive");
         parallelOutput = new MatrixOutputPanel("Parallel");
+        strassenOutput = new MatrixOutputPanel("Strassen");
 
         matrixMulInfoLabel = new JLabel();
         matrixMulInfoLabel.setHorizontalAlignment(SwingConstants.CENTER);
@@ -51,7 +54,9 @@ public class OutputPanel extends JPanel {
         buttonPanel.add(computeParallelButton, gbc);
         gbc.gridx = 0;
         gbc.gridy = 1;
-        gbc.gridwidth = 2;
+        buttonPanel.add(computeStrassenButton, gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 1;
         buttonPanel.add(clearButton, gbc);
         gbc.gridx = 0;
         gbc.gridy = 2;
@@ -63,72 +68,93 @@ public class OutputPanel extends JPanel {
         outputPanel.setLayout(new GridLayout());
         outputPanel.add(naiveOutput);
         outputPanel.add(parallelOutput);
+        outputPanel.add(strassenOutput);
         add(outputPanel, BorderLayout.CENTER);
+    }
+
+    private MultiplicationInput prepareMultiplicationInput() {
+        var A = matrixA.getMatrixValues();
+        var B = matrixB.getMatrixValues();
+
+        if (A == null)
+            return null;
+        if (B == null)
+            return null;
+
+        return new MultiplicationInput(new Benchmark(), A, B, A.length, A[0].length, B.length, B[0].length);
     }
 
     private void addListeners() {
         clearButton.addActionListener(e -> {
             naiveOutput.clear();
             parallelOutput.clear();
+            strassenOutput.clear();
         });
 
         computeNaiveButton.addActionListener(actionEvent -> {
-            var benchmark = new Benchmark();
-            var A = matrixA.getMatrixValues();
-            var B = matrixB.getMatrixValues();
-            if (A == null)
-                return;
-            if (B == null)
+            var input = prepareMultiplicationInput();
+            if (input == null)
                 return;
 
-            int m = A.length, n = A[0].length;
-            int p = B.length, q = B[0].length;
-            matrixMulInfoLabel.setText(String.format("A(%d x %d) * B(%d x %d)\n", m, n, p, q));
+            matrixMulInfoLabel.setText(String.format("A(%d x %d) * B(%d x %d)\n", input.m(), input.n(), input.p(), input.q()));
 
             int[][] result;
             try {
-                var mulMatrix = new Naive(A, B);
-                benchmark.start();
+                var mulMatrix = new Naive(input.A(), input.B());
+                input.benchmark().start();
                 result = mulMatrix.execute();
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(OutputPanel.this, e.getMessage(),
                         "Error Calculate Naive", JOptionPane.ERROR_MESSAGE);
-                benchmark.end();
+                input.benchmark().end();
                 return;
             }
-            benchmark.end();
-            naiveOutput.setResult(Utils.matrixToString(result), benchmark.getExecutionTime());
+            input.benchmark().end();
+            naiveOutput.setResult(Utils.matrixToString(result), input.benchmark().getExecutionTime());
         });
 
         computeParallelButton.addActionListener(actionEvent -> {
-            var benchmark = new Benchmark();
-            int [][] A = matrixA.getMatrixValues();
-            int [][] B = matrixB.getMatrixValues();
-
-            if (A == null)
+            var input = prepareMultiplicationInput();
+            if (input == null)
                 return;
-            if (B == null)
-                return;
+            int divideThreshold = Math.max(input.m(),Math.max(input.n(),input.q())) / 2;
 
-            int m = A.length, n = A[0].length;
-            int p = B.length, q = B[0].length;
-            int divideThreshold = Math.max(m,Math.max(n,q)) / 2;
-
-            matrixMulInfoLabel.setText(String.format("A(%d x %d) * B(%d x %d)\n", m, n, p, q));
+            matrixMulInfoLabel.setText(String.format("A(%d x %d) * B(%d x %d)\n", input.m(), input.n(), input.p(), input.q()));
 
             int [][] result;
             try (var pool = new ForkJoinPool()) {
-                var mulMatrix = new DaC(divideThreshold, A, B, 0, m-1, 0, n-1, 0, p-1, 0, q-1);
-                benchmark.start();
+                var mulMatrix = new DaC(divideThreshold, input.A(), input.B(), 0, input.m()-1, 0, input.n()-1, 0, input.p()-1, 0, input.q()-1);
+                input.benchmark().start();
                 result = pool.invoke(mulMatrix);
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(OutputPanel.this, e.getMessage(),
                         "Error Calculate Naive", JOptionPane.ERROR_MESSAGE);
-                benchmark.end();
+                input.benchmark().end();
                 return;
             }
-            benchmark.end();
-            parallelOutput.setResult(Utils.matrixToString(result), benchmark.getExecutionTime());
+            input.benchmark().end();
+            parallelOutput.setResult(Utils.matrixToString(result), input.benchmark().getExecutionTime());
+        });
+
+        computeStrassenButton.addActionListener(actionEvent -> {
+            var input = prepareMultiplicationInput();
+            if (input == null)
+                return;
+
+            matrixMulInfoLabel.setText(String.format("A(%d x %d) * B(%d x %d)\n", input.m(), input.n(), input.p(), input.q()));
+
+            int [][] result;
+            try {
+                input.benchmark().start();
+                result = Strassen.multiply(input.A(), input.B(), input.m(), input.n(), input.p(), input.q());
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(OutputPanel.this, e.getMessage(),
+                        "Error Calculate Strassen", JOptionPane.ERROR_MESSAGE);
+                input.benchmark().end();
+                return;
+            }
+            input.benchmark().end();
+            strassenOutput.setResult(Utils.matrixToString(result), input.benchmark().getExecutionTime());
         });
     }
 }
